14cde46870dd78dc010261359f941d643bc5b8c3
Barna Tóth <toakak@gmail.com>
2014-08-08 21:59:04 +0200

    Added pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+angular.module('ngGridPanelDemo', ['ngGridPanel'])
+    .controller('mainController', ['$scope', function($scope) {
+        var colors = ['#444444', '#D1DBBD', '#91AA9D', '#3E606F', '#193441', '#703030', '#2F343B', '#7E827A', '#E3CDA4', '#C77966'];
+
+        $scope.items = [];
+
+        for(var i = 0; i < 150; i++) {
+            $scope.items.push({
+                name: 'Click me ' + i,
+                backgroundColor: generateRandomBackgroundColor()
+            });
+        }
+
+        function generateRandomBackgroundColor() {
+            return colors[Math.round(Math.random() * (colors.length - 1))];
+        }
+    }]);
